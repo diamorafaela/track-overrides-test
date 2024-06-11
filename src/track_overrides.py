35b53cd2c9b12d60550b3d53e0913a829a7ba4dd
Francisco Roldan <franciscoproldan@gmail.com>
2024-06-11 10:38:56 -0300

    test

--- src/track_overrides.py
+++ src/track_overrides.py
@@ -49,60 +49,15 @@ def compare_hashes(overrides: List[Dict[str, str]]) -> List[str]:
     for override in overrides:
         latest_commit_hash, patch = get_latest_commit_hash(override['repo'], override['path'], override['method'])
         if latest_commit_hash and latest_commit_hash != override['hash']:
-            output = f"""
+            output = f'''
             <details>
 
             <summary>`{override['method']}` in file `{override['path']}</summary>
 
-            ```python
+            ```diff
             {patch}
             ```
             </details>
-            """
-            output = f'''
-            ensure_unique_roles in file frappe/core/doctype/user/user.py 
-            ```diff
-                            @@ -481,10 +481,9 @@ def after_rename(self, old_name, new_name, merge=False):
-                                has_fields.append(d.get("name"))
-                        for field in has_fields:
-                            frappe.db.sql(
-            -					"""UPDATE `%s`
-            -					SET `%s` = %s
-            -					WHERE `%s` = %s"""
-            -					% (tab, field, "%s", field, "%s"),
-            +					"""UPDATE `{}`
-            +					SET `{}` = {}
-            +					WHERE `{}` = {}""".format(tab, field, "%s", field, "%s"),
-                                (new_name, old_name),
-                            )
-            
-            @@ -527,7 +526,7 @@ def remove_disabled_roles(self):
-            
-                def ensure_unique_roles(self):
-                    exists = []
-            -		for i, d in enumerate(self.get("roles")):
-            +		for _i, d in enumerate(self.get("roles")):
-                        if (not d.role) or (d.role in exists):
-                            self.get("roles").remove(d)
-                        else:
-            @@ -991,7 +990,7 @@ def get_total_users():
-            def get_system_users(exclude_users=None, limit=None):
-                if not exclude_users:
-                    exclude_users = []
-            -	elif not isinstance(exclude_users, (list, tuple)):
-            +	elif not isinstance(exclude_users, list | tuple):
-                    exclude_users = [exclude_users]
-            
-                limit_cond = ""
-            @@ -1081,7 +1080,7 @@ def handle_password_test_fail(feedback: dict):
-                suggestions = feedback.get("suggestions", [])
-                warning = feedback.get("warning", "")
-            
-            +	frappe.throw(msg=" ".join([warning, *suggestions]), title=_("Invalid Password"))
-            
-            
-            def update_gravatar(name):
-            ```
             '''
             changed_methods.append(output)
     return changed_methods
